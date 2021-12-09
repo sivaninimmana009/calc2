a9e84844d9980cdebdc8bcabaecd5bb85225ef8d
mohammedsaif <95833215+sivaninimmana009@users.noreply.github.com>
2021-12-09 02:54:26 +0000

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -20,10 +20,6 @@ class Calculator:
         self.result = value_a * value_b
         return self.result
 
-    def divide_numbers(self,value_a,value_b):
-        """ divide two numbers and store the result """
-        self.result = value_a/value_b
-        return self.result
 
 
 
